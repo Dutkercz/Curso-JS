9f912233e284a19b7b2537dce387b2a44c8ebf98
Dutkercz <dutkercz@gmail.com>
2025-04-11 18:40:09 -0300

    Tabuada final

--- Ex-04/ex04.js
+++ Ex-04/ex04.js
@@ -1,7 +1,17 @@
-var res = document.getElementById("res")
-var numero = document.getElementById("ini")
 function tabuada(){
-    for(var c = 1; c <= 10; c++){
-        res.innerHTML = `${numero} x ${c} = ${numero * c}`
+    var numero = document.getElementById("ini")
+    var select = document.getElementById("sel")
+
+    if(numero.value.length < 1){
+        alert("[ERROR] Não foi digitado nenhum numero")
+    }else{
+        for(var c = 1; c <= 10; c++){
+            var item = document.createElement("option")
+            item.text = `${numero.value} x ${c} = ${numero.value * c}`
+            select.appendChild(item)
+        }
+
+
     }
+    
 }
